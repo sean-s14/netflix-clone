0edd979e3877448e7e91675a37273ad931fc4685
Sean Stocker <sean.stocker14@gmail.com>
2023-09-06 18:23:58 +0100

    feat: Integrate mongoose, and custom utility package
    Additions:
    - Installed 'mongoose' for MongoDB interactions
    - Integrated custom utility package '@sean14/utils'
    - Created 'mongoConnect' for MongoDB connection handling
    - Defined global mongoose variable in 'global.d.ts' for universal API usage

---
+++ src/utils/mongoConnect.ts
@@ -0,0 +1,42 @@
+import mongoose, { ConnectOptions } from "mongoose";
+
+const URI = process.env.MONGO_URI;
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connect() {
+  if (!URI) {
+    throw new Error(
+      "Please define the URI environment variable inside .env.local"
+    );
+  }
+
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(URI, {
+        bufferCommands: false,
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      } as ConnectOptions)
+      .then((mongoose) => {
+        console.log("Connected to MongoDB");
+        return mongoose;
+      })
+      .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+      });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+export default connect;
